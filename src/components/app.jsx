f5de85adb27f8370f2ca5642b9fefaf27ad10b43
Alexis Lenoir <alexis59495@gmail.com>
2020-06-23 23:51:34 -0300

    Create the first components

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBar from './search_bar';
+
+const App = () => {
+  return (
+    <div>
+      <div className="left-scene">
+        <SearchBar />
+      </div>
+      <div className="right-scene">
+      </div>
+    </div>
+  );
+};
+
+export default App;
